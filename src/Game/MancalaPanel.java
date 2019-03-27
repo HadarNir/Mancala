11caef7f53ce3c2425bf2cb71b2190805f3567e9
HadarNir <hadar4nir@gmail.com>
2019-03-27 22:00:31 +0200

    starting to fix network bugs

--- src/Game/MancalaPanel.java
+++ src/Game/MancalaPanel.java
@@ -32,11 +32,6 @@ public class MancalaPanel extends JPanel {
         g2.draw(new RoundRectangle2D.Double(25, 0, GameBoard.MANCALA_WIDTH, GameBoard.MANCALA_HEIGHT, 80, 150));
         g2.setColor(Color.BLACK);
         g2.drawString(this.stoneAmount + " Player " + Character.toUpperCase(playerMancala), 50, GameBoard.MANCALA_HEIGHT + 20);
-        if (stones != null)
-            for (JLabel image : this.stones) {
-                if (image != null)
-                    this.add(image);
-            }
     }
 
     public void setStoneAmount(int stoneAmount) {
@@ -53,6 +48,7 @@ public class MancalaPanel extends JPanel {
         int centerY = r.nextInt(highY - lowY) + lowY;
         imageLabel.setBounds(centerX, centerY, imageLabel.getPreferredSize().width, imageLabel.getPreferredSize().height);
         this.stones.add(imageLabel);
+        this.add(imageLabel);
         this.repaint();
     }
 }
