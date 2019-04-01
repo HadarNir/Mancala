0909530e8c22d25c87b007bf12132cd4130c3acc
HadarNir <hadar4nir@gmail.com>
2019-04-01 17:01:18 +0300

    adding gui for users

---
+++ src/Game/MainPage.java
@@ -0,0 +1,61 @@
+package Game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainPage {
+    public MainPage(MancalaClient m) {
+        //create a frame
+        JFrame frame = new JFrame();
+        // Create a panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        // Create component for panel
+        JLabel topic = new JLabel("<html>Welcome To <br/>Mancala Online</html>");
+        topic.setFont(new Font("Serif", Font.PLAIN, 30));
+        JLabel id = new JLabel("please enter your name");
+        id.setFont(new Font("Serif", Font.PLAIN, 20));
+        JTextField name = new JTextField();
+        JButton presentButton = new JButton("play");
+        // Add components to panel
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.WEST;
+        c.weightx = 0.1;
+        c.weighty = 0.1;
+
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(topic, c);
+
+
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(id, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        panel.add(name, c);
+
+
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(presentButton, c);
+        c.gridx = 1;
+        c.gridy = 2;
+        // Add panel to frame
+        frame.add(panel);
+        frame.setSize(600, 600);
+        frame.setTitle("Mancala");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        // Create anonymous classes
+        presentButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
